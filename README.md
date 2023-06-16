dcf664d430e3d3edf6b4a77d8ec02e2319b5a7f8
Djoufson <djouflegran@gmail.com>
2023-06-16 13:21:17 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Saga Demo
---
+++ src/SagaDemo.OrderService/Controllers/OrdersController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace SagaDemo.OrderService.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class OrdersController : ControllerBase
+{
+
+}
